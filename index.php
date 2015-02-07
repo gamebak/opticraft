6f74ac5958bb3ea93babb12373e2533dcad0d1f7
gamebak <alexandru.cobuz@gmail.com>
2015-02-07 14:26:07 +0200

    - update

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php
-
-
 require_once("config.php");
+
+/**Init template class*/
 $template = new Template;
+
 
 $template->title="Opticraft";
 echo $template->renderHead();
 
+echo "<h1>".$template->title."</h1>";
 
 echo $template->renderFooter();
 
-echo "<h1>".$template->title."</h1>";
