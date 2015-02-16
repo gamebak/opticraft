4ab54f190b2ca5fdab8705c65a5f173cf61f508f
gamebak <alexandru.cobuz@gmail.com>
2015-02-16 23:21:26 +0200

    - index html to template

--- class/template.php
+++ class/template.php
@@ -5,7 +5,25 @@
  */
 class Template
 {
-	public $title, $description;
+	/**
+	 * @var string $title          Page Title
+	 */
+	public $title;
+
+	/**
+	 * @var string $description    Page meta description
+	 */
+	public $description;
+
+	/**
+	 * @var array $scriptSrc 	    Inject scripts src inside scriptRender from array
+	 */
+	public $scriptSrc = array();
+
+	/**
+	 * @var  array $cssHref 		Inject css scripts in the header
+	 */
+	public $cssHref = array();
 
 	/**
 	 * Render the html head
@@ -14,7 +32,23 @@ class Template
 	*/
 	public function renderHead()
 	{
-		return '<html><head><title>'.$this->title.'</title></head><body>';
+		$tmp = '<!DOCTYPE html>
+		<html lang="en"><head>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+		<meta charset="utf-8">
+		<title>'.$this->title.'</title>';
+
+		// Am facut spatierea asta ca sa faci tu "hack-ul" pentru partea de css
+		$tmp.='<meta name="generator" content="Bootply" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css" rel="stylesheet">
+		<!--[if lt IE 9]>
+			<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+		<link href="css/styles.css" rel="stylesheet"></head><body>';
+
+		return $tmp;
 	}
 
 	/**
@@ -25,5 +59,129 @@ class Template
 	public function renderFooter()
 	{
 		return '</body></html>';
+	}
+
+	/**
+	 * Load scripts dynamically and render them in page
+	 * 
+	 * @return string|void    String with scripts code
+	 */
+	public function scriptsRender()
+	{
+		if(count($this->scriptSrc) > 0 )
+		{
+			$tmpString = '';
+
+			for($i=0; $i<count($this->scriptSrc); $i++)
+			{
+				$tmpString.= '<script src="'.$this->scriptSrc[$i].'"><script>';
+			}
+
+			return $tmpString;
+		}
+	}
+
+	/**
+	 * @To Do - Aceeasi logica precum scriptsRender(), doar ca trebuie sa pui link href, va fi in header (putin mai complicat)
+	 * @return [type] [description]
+	 */
+	public function cssRender()
+	{
+		if(count($this->cssHref) > 0 )
+		{
+
+		}
+	}
+
+	/**
+	 * Render social buttons
+	 */
+	public function renderSocial()
+	{
+		return '<div class="row">
+        <div class="col-lg-12 text-center v-center" style="font-size:39pt;">
+          <a href="#"><i class="icon-google-plus"></i></a> <a href="#"><i class="icon-facebook"></i></a>  <a href="#"><i class="icon-twitter"></i></a> <a href="#"><i class="icon-github"></i></a> <a href="#"><i class="icon-pinterest"></i></a>
+        </div>
+      </div>';
+	}
+
+	public function mainContainer()
+	{
+		return '<div class="container-full">
+
+      <div class="row">
+       
+        <div class="col-lg-12 text-center v-center">
+          
+          <h1>Welcome to Opticraft</h1>
+          <p class="lead">A simple and efficient optimization tool for your web-site</p>
+          
+          <br><br><br>
+          
+          <form class="col-lg-12">
+            <div class="input-group" style="width:340px;text-align:center;margin:0 auto;">
+            <input class="form-control input-lg" placeholder="Enter your URL" type="text">
+              <span class="input-group-btn"><button class="btn btn-lg btn-primary" type="button">OK</button></span>
+            </div>
+          </form>
+        </div>
+        
+      </div> <!-- /row -->
+  	  '.$this->renderSocial().'
+  
+  	<br><br><br><br><br>
+
+	</div> <!-- /container full -->';
+	}
+
+	/**
+	 * Continuarea cu al doilea container, nu am fost inspirat ca-s obosit
+	 * @return [type] [description]
+	 */
+	public function secondContainer()
+	{
+		return '<div class="container">
+  		<hr>
+	  	<div class="row">
+	        <div class="col-md-4">
+	          <div class="panel panel-default">
+	            <div class="panel-heading"><h3>Hello.</h3></div>
+	            <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
+	            Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
+	            dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
+	            Aliquam in felis sit amet augue.
+	            </div>
+	          </div>
+	        </div>
+	      	<div class="col-md-4">
+	        	<div class="panel panel-default">
+	            <div class="panel-heading"><h3>Hello.</h3></div>
+	            <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
+	            Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
+	            dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
+	            Aliquam in felis sit amet augue.
+	            </div>
+	          </div>
+	        </div>
+	      	<div class="col-md-4">
+	        	<div class="panel panel-default">
+	            <div class="panel-heading"><h3>Hello.</h3></div>
+	            <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
+	            Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
+	            dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
+	            Aliquam in felis sit amet augue.
+	            </div>
+	          </div>
+	        </div>
+	    </div>
+	  
+		<div class="row">
+	        <div class="col-lg-12">
+	        <br><br>
+	          <p class="pull-right"><a href="http://www.bootply.com">Template from Bootply</a> &nbsp; ©Copyright 2013 ACME<sup>TM</sup> Brand.</p>
+	        <br><br>
+	        </div>
+	  </div>
+	</div>';
 	}
 }
--- index.php
+++ index.php
@@ -1,117 +1,44 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-		<meta charset="utf-8">
-		<title>Opticraft</title>
-		<meta name="generator" content="Bootply" />
-		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-		<link href="css/bootstrap.min.css" rel="stylesheet">
-		<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css" rel="stylesheet">
-		<!--[if lt IE 9]>
-			<script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
-		<![endif]-->
-		<link href="css/styles.css" rel="stylesheet">
-	</head>
-	<body>
-<div class="container-full">
-
-      <div class="row">
-       
-        <div class="col-lg-12 text-center v-center">
-          
-          <h1>Welcome to Opticraft</h1>
-          <p class="lead">A simple and efficient optimization tool for your web-site</p>
-          
-          <br><br><br>
-          
-          <form class="col-lg-12">
-            <div class="input-group" style="width:340px;text-align:center;margin:0 auto;">
-            <input class="form-control input-lg" placeholder="Enter your URL" type="text">
-              <span class="input-group-btn"><button class="btn btn-lg btn-primary" type="button">OK</button></span>
-            </div>
-          </form>
-        </div>
-        
-      </div> <!-- /row -->
-  
-  	  <div class="row">
-       
-        <div class="col-lg-12 text-center v-center" style="font-size:39pt;">
-          <a href="#"><i class="icon-google-plus"></i></a> <a href="#"><i class="icon-facebook"></i></a>  <a href="#"><i class="icon-twitter"></i></a> <a href="#"><i class="icon-github"></i></a> <a href="#"><i class="icon-pinterest"></i></a>
-        </div>
-      
-      </div>
-  
-  	<br><br><br><br><br>
-
-</div> <!-- /container full -->
-
-<div class="container">
-  
-  	<hr>
-  
-  	<div class="row">
-        <div class="col-md-4">
-          <div class="panel panel-default">
-            <div class="panel-heading"><h3>Hello.</h3></div>
-            <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
-            Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
-            dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
-            Aliquam in felis sit amet augue.
-            </div>
-          </div>
-        </div>
-      	<div class="col-md-4">
-        	<div class="panel panel-default">
-            <div class="panel-heading"><h3>Hello.</h3></div>
-            <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
-            Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
-            dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
-            Aliquam in felis sit amet augue.
-            </div>
-          </div>
-        </div>
-      	<div class="col-md-4">
-        	<div class="panel panel-default">
-            <div class="panel-heading"><h3>Hello.</h3></div>
-            <div class="panel-body">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis pharetra varius quam sit amet vulputate. 
-            Quisque mauris augue, molestie tincidunt condimentum vitae, gravida a libero. Aenean sit amet felis 
-            dolor, in sagittis nisi. Sed ac orci quis tortor imperdiet venenatis. Duis elementum auctor accumsan. 
-            Aliquam in felis sit amet augue.
-            </div>
-          </div>
-        </div>
-    </div>
-  
-	<div class="row">
-        <div class="col-lg-12">
-        <br><br>
-          <p class="pull-right"><a href="http://www.bootply.com">Template from Bootply</a> &nbsp; ©Copyright 2013 ACME<sup>TM</sup> Brand.</p>
-        <br><br>
-        </div>
-    </div>
-</div>
 
 
-	<!-- script references -->
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
-		<script src="js/bootstrap.min.js"></script>
-	</body>
-</html>
 
 
-// <?php
-// require_once("config.php");
+
+
+	
+		
+
+
+
+<?php
+require_once("config.php");
 
 // *Init template class
-// $template = new Template;
+$template = new Template;
+
+/**
+ * Trebuie sa creezi array-ul cssHref inainte de renderHead ( pentru ca acolo sunt afisate, trebuie incarcate inainte )
+ */
+
+$template->title="Opticraft";
+echo $template->renderHead();
 
 
-// $template->title="Opticraft";
-// echo $template->renderHead();
+echo $template->mainContainer();
+echo $template->secondContainer();
 
-// echo "<h1>".$template->title."</h1>";
+/**
+ * Load javascript files dynamically
+ */
+$template->scriptSrc[] = "//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js";
+$template->scriptSrc[] = "js/bootstrap.min.js";
 
-// echo $template->renderFooter();
+/**
+ * This will output:
+ * <script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+ *  <script src="js/bootstrap.min.js"></script>
+ */
+echo $template->scriptsRender();
+
+
+echo $template->renderFooter();
 
