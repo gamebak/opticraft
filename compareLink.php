ec7e4d50712f906bee17d448ff356456d5ef61ab
bianca23 <bianca_vulpe@yahoo.com>
2015-02-25 14:46:21 +0200

    - url validation (regex)

--- compareLink.php
+++ compareLink.php
@@ -2,31 +2,22 @@
 
 require_once("config.php");
 
-
 $r = new Request;
 $url = $_POST["urlInput"];
 
-//url filter to verify the validity of the url
-$var = filter_var($url, FILTER_VALIDATE_URL);
+//url filter to verify the validity of the url using regex
+$pattern = '_^(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\x{00a1}-\x{ffff}0-9]-*)*[a-z\x{00a1}-\x{ffff}0-9]+)(?:\.(?:[a-z\x{00a1}-\x{ffff}0-9]-*)*[a-z\x{00a1}-\x{ffff}0-9]+)*(?:\.(?:[a-z\x{00a1}-\x{ffff}]{2,})))(?::\d{2,5})?(?:/\S*)?$_iuS';
+$success = preg_match($pattern, $url, $match);
 
-/*if($var) {
-	//verify the use of http://
-	$str = substr($url, 0, 7);
-	if($str!="http://") {
-		echo "Please use http:// in front of the URL!";
-	} else {
-		$data = $r->fetch($url);
-		var_dump($data);
-	}
+if ($success) {
+	$data = $r->fetch($url);
+	var_dump($data);
 } else {
-	echo "The URL does not exist!";
-}*/
-
-if($var) {
-	echo "este url";
-} else {
-	echo "nu este url";
+	echo "Please insert a valid URL!";
 }
+
+
+
 
 
 
