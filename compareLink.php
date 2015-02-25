707e3ade99a861c160d0d9aee73bc87f0567861c
bianca23 <bianca_vulpe@yahoo.com>
2015-02-25 16:56:04 +0200

    -

--- compareLink.php
+++ compareLink.php
@@ -7,7 +7,7 @@ $url = $_POST["urlInput"];
 
 //url filter to verify the validity of the url using regex
 $pattern = '_^(?:(?:https?|ftp)://)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\x{00a1}-\x{ffff}0-9]-*)*[a-z\x{00a1}-\x{ffff}0-9]+)(?:\.(?:[a-z\x{00a1}-\x{ffff}0-9]-*)*[a-z\x{00a1}-\x{ffff}0-9]+)*(?:\.(?:[a-z\x{00a1}-\x{ffff}]{2,})))(?::\d{2,5})?(?:/\S*)?$_iuS';
-$success = preg_match($pattern, $url, $match);
+$success = preg_match($pattern, $url);
 
 if ($success) {
 	$data = $r->fetch($url);
