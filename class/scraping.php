5f49e1c165213ac7935beb369b907e89cd60fe2e
bianca23 <bianca_vulpe@yahoo.com>
2015-04-01 18:25:54 +0300

    -blah

--- class/scraping.php
+++ class/scraping.php
@@ -21,52 +21,6 @@ class Scrape
 	/** @var string $domainName main domain in the current scraping object */
 	public $domainName = '';
 
-	/**
-	* Scrape the website for css files
-	*
-	* @param string $data content of the website
-	*
-	* @return string
-	*/	
-	public function scrapeCss($data) 
-	{
-		$match = preg_match_all('~href=\'.*\.css\'~', $data, $arrayCss);
-		return $arrayCss;
-	}	
-
-	/**
-	* Scrape the website for javascript files
-	*
-	* @param string $data content of the website
-	*
-	* @return string
-	*/	
-	public function scrapeJs($data) 
-	{
-		$match = preg_match_all('~src=".*\.js"~', $data, $arrayJs);
-		return $arrayJs;
-	}	
-
-	/**
-	* Scrape the website for images
-	*
-	* @param string $data content of the website
-	*
-	* @return boolean|string  		returns false if nothing found
-	*/	
-	public function scrapeImg($data) 
-	{
-		$match = preg_match_all('~src="(.*?(\.jpg|\.gif|\.png|\.jpeg|\.bmp))"~', $data, $arrayImg);
-		// no results, or no images found
-		if(!$match || !isset($arrayImg[1][0]))
-			return false;
-
-		$arrFilteredResults = $arrayImg[1];
-
-		// cleanup
-		unset($arrayImg, $match);
-		return $arrFilteredResults;
-	}
 
 	/**
 	 * Filter elements in array based on the domain path
@@ -81,24 +35,75 @@ class Scrape
 
 		// here we will save every ressource
 		$arrValidLocal = array();
-
 		for($i = 0; $i<count($arr); $i++)
 		{
-			//if external url, it must start with http
-			if(substr($arr[$i], 0,4) == 'http')
+			//if external url, it must start with http or have www. in it
+			$str = (string)$arr[0][$i];
+			/*if(strstr($str, 'http') || strstr($str, 'https') || strstr($str, 'www'))
 			{
-				// check if is $this->domainName the main url
-				
+				if($this->domainName == strstr($str, '.', true))
+				 	$arrValidLocal[] = $arr[$i];	
+				echo "e dintr-un site!!! ". $str;			
 			}
 			else
 			{
 				//not external, then add it to valid local array
 				$arrValidLocal[] = $arr[$i];
 			}
+			return $arrValidLocal;
+			
 		}
 
 		// return only valid ressources that are local, without complete domain path in url
-		return $arrValidLocal;
+		return $arrValidLocal;*/
+
 	}
+
+	/**
+	* Scrape the website for css files
+	*
+	* @param string $data content of the website
+	*
+	* @return string
+	*/	
+	public function scrapeCss($data) 
+	{
+		$match = preg_match_all('~[a-zA-Z0-9:/_.-]+\.css~', $data, $arrayCss);
+
+		return $this->filterLocalStrings($arrayCss);
+		//return $arrayCss;
+	}	
+
+	/**
+	* Scrape the website for javascript files
+	*
+	* @param string $data content of the website
+	*
+	* @return string
+	*/	
+	public function scrapeJs($data) 
+	{
+		$match = preg_match_all('~[a-zA-Z0-9:/_.-]+\.js~', $data, $arrayJs);
+
+		return $this->filterLocalStrings($arrayJs);
+		//return $arrayJs;
+	}	
+
+	/**
+	* Scrape the website for images
+	*
+	* @param string $data content of the website
+	*
+	* @return boolean|string  		returns false if nothing found
+	*/	
+	public function scrapeImg($data) 
+	{
+		$match = preg_match_all('~[a-zA-Z0-9:/_.-]+(\.jpg|\.gif|\.png|\.jpeg|\.bmp)~', $data, $arrayImg);
+		// no results, or no images found
+
+		return $this->filterLocalStrings($arrayImg);
+		//return $arrayImg;
+	}
+
 }	
 
